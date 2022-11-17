7cb4f55831b1964bd736ae23d4758508aca2dc9a
Administrator <Administrator@DESKTOP-4EMBS87>
2022-11-17 22:24:09 +0800

    AOP XMl 启动注解

--- spring-demo/src/main/java/com/kpb/spring/SpringXmlAopDemo.java
+++ spring-demo/src/main/java/com/kpb/spring/SpringXmlAopDemo.java
@@ -4,6 +4,7 @@ import com.kpb.spring.aop.IAccountService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
 public class SpringXmlAopDemo {
 
 	public static void main(String[] args) {
--- spring-demo/src/main/java/com/kpb/spring/aop/Logger.java
+++ spring-demo/src/main/java/com/kpb/spring/aop/Logger.java
@@ -6,11 +6,12 @@ import org.aspectj.lang.ProceedingJoinPoint;
  * 用户记录日志的工具类，里面提供公共的代码
  */
 public class Logger {
+
 	/**
 	 * 用于打印日志：计划让其在切入点方法执行前执行（切入点方法就是业务层方法）
 	 */
 	public void beforePrintLog(){
-		System.out.println("Logger类中的pringLog方法开始记录日志了。。。");
+		System.out.println("Logger类中的printLog方法开始记录日志了。。。");
 	}
 	public void afterReturningPrintLog()
 	{
